629bc62765fb50f19cea21281b6fe1f04e59a365
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-07-12 19:33:27 -0500

    merge common controllers into parent class

--- Assets/Scripts/Controller/EditController.cs
+++ Assets/Scripts/Controller/EditController.cs
@@ -2,24 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EditController : StateMachine, IPersistence {
-  [SerializeField] GameObject selectionIndicatorPrefab;
-
-  private Transform _marker;
-  public Transform marker {
-    get {
-      if (_marker == null) {
-        GameObject instance = Instantiate(selectionIndicatorPrefab) as GameObject;
-        instance.transform.SetParent(transform);
-        _marker = instance.transform;
-      }
-      return _marker;
-    }
-  }
-  public Point pos;
-  public Grid grid;
-  public UnitManager units;
-  public string mapToLoad;
+public class EditController : StateMachineBoard {
   public MapData mapData {
     get {
       if (_mapData == null) mapData = new MapData();
--- Assets/Scripts/Controller/StageController.cs
+++ Assets/Scripts/Controller/StageController.cs
@@ -2,31 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StageController : StateMachine, IPersistence {
-  [SerializeField] GameObject selectionIndicatorPrefab;
-
-  private Transform _marker;
-  public Transform marker {
-    get {
-      if (_marker == null) {
-        GameObject instance = Instantiate(selectionIndicatorPrefab) as GameObject;
-        instance.transform.SetParent(transform);
-        _marker = instance.transform;
-      }
-      return _marker;
-    }
-  }
-  private Point _pos;
-  public Point pos {
-    get { return _pos; }
-    set { _pos = value; marker.position = new Vector3(_pos.x, _pos.y); }
-  }
-  public Grid grid;
-  public UnitManager units;
+public class StageController : StateMachineBoard {
   public ActionPointsManager apManager;
-  public IPersistence persistence { get { return (IPersistence)this; } }
-
-  public string mapToLoad;
 
   void Start() {
     Load(mapToLoad);
